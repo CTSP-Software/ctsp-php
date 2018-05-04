afa95e03755a5cbab71bdb21b8927d7b74b36263
Pedro Kiyuna <a16013@bcc.unifal-mg.edu.br>
2018-05-04 11:49:54 -0300

    modified new/first

---
+++ app/User.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pedro
+ * Date: 5/2/2018
+ * Time: 4:38 PM
+ */
+
+namespace App;
+
+
+class User
+{
+
+}
---
+++ resources/views/projects/import_project.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pedro
+ * Date: 5/4/2018
+ * Time: 11:45 AM
+ */
