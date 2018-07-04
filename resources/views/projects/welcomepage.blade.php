eb33053c9c03c7710c9a1ccdb6724f8b6ba73b91
Pedro Kiyuna <a16013@bcc.unifal-mg.edu.br>
2018-07-04 09:33:17 -0300

    teste para verificar se esta sendo feito push para o git hub. Apenas inclui um comentario no codigo

--- resources/views/projects/welcomepage.blade.php
+++ resources/views/projects/welcomepage.blade.php
@@ -13,6 +13,7 @@
             <a href="#" class="list-group-item list-group-item-action">2 - Assing Team Members To Scrum Roles</a>
             <a href="#" class="list-group-item list-group-item-action">3 - Create a Product Backlog for the Project</a>
             <a href="#" class="list-group-item list-group-item-action disabled">4 - Create a Sprint</a>
+            <!-- Pagliares -->
         </div>
     </div>
 </div>
